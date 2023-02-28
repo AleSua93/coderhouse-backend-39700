5483bd09f618a79274acacb66c1cb89e385686bb
Alejandro Suarez <alejandronicolassuarez@hotmail.com>
2023-02-28 20:46:42 -0300

    clase 9 - ejemplo avanzado handlebars

--- clase_9/handlebars_ejemplo/src/app.js
+++ clase_9/handlebars_ejemplo/src/app.js
@@ -1,21 +1,17 @@
 import express from "express";
 import { engine } from "express-handlebars";
-import __dirname from "./utils.js";
+import { __dirname } from "./utils.js";
+import viewsRouter from "./routes/views.router.js";
 
 const app = express();
+
+app.use(express.static(__dirname + "/../public"));
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => {
-  const user = {
-    firstName: "Franco",
-    lastName: "Jalil",
-  };
-
-  res.render("home", user);
-});
+app.use("/", viewsRouter);
 
 app.listen(8080, () => {
   console.log("Server listening on port 8080");
