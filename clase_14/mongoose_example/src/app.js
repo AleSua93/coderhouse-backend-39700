3b9fe94144412a62a964a97972c1c91add7eee92
Alejandro Suarez <alejandronicolassuarez@hotmail.com>
2023-03-16 21:09:11 -0300

    clase 14 - Actividad en clase

--- clase_14/mongoose_example/src/app.js
+++ clase_14/mongoose_example/src/app.js
@@ -10,10 +10,6 @@ app.listen(8080, () => {
   console.log("Server listening on port 8080");
 });
 
-mongoose
-  .connect(
-    "mongodb+srv://Coder:mipassword12@codercluster.kvisivd.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then((conn) => {
-    console.log("Connected to DB!");
-  });
+mongoose.connect("connection-string").then((conn) => {
+  console.log("Connected to DB!");
+});
